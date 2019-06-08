edbae455834a93e01443aa9860734655ff41abb7
Gunharth Randolf <hello@gunharth.io>
2019-06-08 14:40:35 +0200

    Add sw and manifest

---
+++ docs/sw.js
@@ -0,0 +1,68 @@
+const swCache = 'leafletpg-v1';
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(swCache)
+            .then(function (cache) {
+                return cache.addAll([
+                    '/',
+                    'index.html',
+                    'css/leaflet.css',
+                    'css/style.css',
+                    'js/leaflet.js',
+                    'favicons/icon-512x512.png',
+                    'favicons/android-chrome-192x192.png',
+                    'favicons/android-chrome-384x384.png',
+                    'favicons/apple-touch-icon.png',
+                    'favicons/favicon-16x16.png',
+                    'favicons/favicon-32x32.png',
+                    'favicons/mstile-150x150.png',
+                    'favicons/safari-pinned-tab.svg',
+                    'images/layers-2x.png',
+                    'images/layers.png',
+                    'images/marker-icon-2x.png',
+                    'images/marker-icon.png',
+                    'images/marker-shadow.png',
+                    'leaflet-tilelayer-wmts.js',
+                    'site.webmanifest'
+                ]);
+            })
+    );
+});
+
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== swCache) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
+
+// self.addEventListener('fetch', function (event) {
+//     let online = navigator.onLine
+//     if (!online) {
+//         event.respondWith(
+//             caches.match(event.request).then(function (res) {
+//                 if (res) {
+//                     return res;
+//                 }
+//                 requestBackend(event);
+//             })
+//         )
+//     }
+// });
